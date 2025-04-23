80060e4d7ea97ebde73320177250246b2afe5618
matthewhiggins415 <mhiggins.louis@gmail.com>
2025-04-23 11:58:59 -0700

    Altered origin.

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ connectDB();
 
 // app.use(cors());
 
-app.use(cors({ origin: ['https://angrytaters.com', 'http://localhost:3000', 'https://stripe.com'] }))
+app.use(cors({ origin: ['https://buddyspud.com', 'http://localhost:3000', 'https://stripe.com'] }))
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://angrytaters.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://buddyspud.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
